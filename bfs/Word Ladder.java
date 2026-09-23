73234c0209d4620aa522446bbec836ac41d53105
Akan Abdireshov <asanovich.02@gmail.com>
2026-09-23 09:48:45 -0700

    Word Ladder

---
+++ bfs/Word Ladder.java
@@ -0,0 +1,44 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        if (!words.contains(endWord)) {
+            return 0;
+        }
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+        Set<String> visited = new HashSet<>();
+        visited.add(beginWord);
+        int level = 1;
+
+        while (!queue.isEmpty()) {
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                String current = queue.poll();
+                if (current.equals(endWord)) {
+                    return level;
+                }
+                char[] chars = current.toCharArray();
+                for (int j = 0; j < chars.length; j++) {
+                    char original = chars[j];
+                    for (char c = 'a'; c <= 'z'; c++) {
+                        if (c == original) {
+                            continue;
+                        }
+                        chars[j] = c;
+                        String next = new String(chars);
+
+                        if (words.contains(next) && !visited.contains(next)) {
+                            visited.add(next);
+                            queue.offer(next);
+                        }
+                    }
+                    chars[j] = original;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
